d77a85968865b0add52563e51f842283e90b1061
Phil Wang <lucidrains@gmail.com>
2020-10-05 11:36:22 -0700

    complete global content attention, sans relative positional attention

--- README.md
+++ README.md
@@ -1,12 +1,35 @@
 <img src="./gsa.png" width="500px"></img>
 
-## Global Self-attention Network
+## Global Self-attention Network (wip)
 
 An implementation of <a href="https://openreview.net/forum?id=KiFeuZu24k">Global Self-Attention Network</a>, which proposes an all-attention vision backbone that achieves better results than convolutions with less parameters and compute.
 
 They use a previously discovered <a href="https://arxiv.org/abs/1812.01243">linear attention variant</a> with a small modification for further gains (no normalization of the queries), paired with relative positional attention, computed axially for efficiency.
 
 The result is an extremely simple circuit composed of 7-8 einsums, 1 softmax, and normalization.
+
+## Install
+
+```bash
+$ pip install gsa-pytorch
+```
+
+## Usage
+
+```python
+import torch
+from gsa_pytorch import GSA
+
+gsa = GSA(
+    dim = 3,
+    dim_out = 64,
+    dim_key = 32,
+    heads = 8
+)
+
+x = torch.randn(1, 3, 256, 256)
+gsa(x) # (1, 64, 256, 256)
+```
 
 ## Citations
 
--- gsa_pytorch/gsa_pytorch.py
+++ gsa_pytorch/gsa_pytorch.py
@@ -1,10 +1,47 @@
 import torch
-from torch import nn
+from torch import nn, einsum
 from einops import rearrange
 
-class GSA(nn.Module):
-    def __init__(self):
-        super().__init__()
+# helpers
 
-    def forward(self, x):
-        return x
+def default(val, d):
+    return val if exists(val) else d
+
+def exists(val):
+    return val is not None
+
+# classes
+
+class GSA(nn.Module):
+    def __init__(self, dim, *, rel_pos_length = None, dim_out = None, heads = 8, dim_key = 64):
+        super().__init__()
+        dim_out = default(dim_out, dim)
+        dim_hidden = dim_key * heads
+
+        self.heads = heads
+        self.dim_out = dim_out
+        self.rel_pos_length = rel_pos_length
+
+        self.to_qkv = nn.Conv2d(dim, dim_hidden * 3, 1, bias = False)
+        self.to_out = nn.Conv2d(dim_hidden, dim_out, 1)
+
+        if exists(rel_pos_length):
+            self.rel_columns = nn.Parameter(torch.randn(rel_pos_length, dim_key))
+            self.rel_rows = nn.Parameter(torch.randn(rel_pos_length, dim_key))
+
+    def forward(self, img):
+        b, c, x, y, h, c_out = *img.shape, self.heads, self.dim_out
+
+        qkv = self.to_qkv(img).chunk(3, dim = 1)
+        q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h = h), qkv)
+
+        k = k.softmax(dim = -1)
+        context = einsum('bhdn,bhen->bhde', k, v)
+
+        content_out = einsum('bhde,bhdn->bhen', context, q)
+        content_out = content_out.reshape(b, -1, x, y)
+        content_out = self.to_out(content_out)
+
+        # todo: compute relative position attentions and sum to content_out
+
+        return content_out
